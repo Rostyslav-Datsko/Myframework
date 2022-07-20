5b5added9d735d371aab513cf4ae504e7f32a5cc
Rostyslav Datsko <datskorostislav@gmail.com>
2022-07-21 00:32:23 +0300

    Add classes App & Registry

---
+++ config/params.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'admin_email' => 'datskorostislav@gmail.com',
+    'site_name' => 'E-shop',
+    'pagination' => 6,
+
+];
--- public/index.php
+++ public/index.php
@@ -2,3 +2,9 @@
 
 require_once dirname(__DIR__) . '/config/init.php';
 
+new \wfm\App();
+\wfm\App::$app->setProperty('test', 'test');
+var_dump(\wfm\App::$app->getProperties());
+
+echo \wfm\App::$app->getProperty('pagination');
+
