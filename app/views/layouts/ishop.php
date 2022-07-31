6dbf0781555f26fb042bd82130023b407aa21e0b
Rostyslav Datsko <datskorostislav@gmail.com>
2022-07-31 04:16:13 +0300

    Add layout in public/assets and create construction of /layout/ishop

--- app/views/layouts/ishop.php
+++ app/views/layouts/ishop.php
@@ -2,6 +2,8 @@
 use wfm\View;
 /** @var $this View*/
 ?>
-<?php $this->getPart('parts/header');?>
-<?= $this->content ?>
-<?php $this->getPart('parts/footer');?>
+<?php $this->getPart('parts/header'); ?>
+<?php echo $this->content?>
+
+
+<?php $this->getPart('/parts/footer'); ?>
