0c00ee21d69855e83353e2d9335b60f73a3a5233
Rostyslav Datsko <datskorostislav@gmail.com>
2022-08-02 20:13:25 +0300

    Create parts/products_loop. And models/Main function get_hits

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -12,7 +12,12 @@ class MainController extends AppController
     public function indexAction()
     {
         $slides = R::findAll('slider');
-        $this->set(compact('slides'));
+
+        $products = $this->model->get_hits(1, 5);
+
+
+
+        $this->set(compact('slides', 'products'));
     }
 
 }
--- app/models/Main.php
+++ app/models/Main.php
@@ -3,7 +3,17 @@
 namespace app\models;
 
 
+use RedBeanPHP\R;
+
 class Main extends AppModel
 {
+
+    //вибрати все з таблиці product і product_description зєднай 2 таблиці де product.id = product_description.product_id
+    //з статусом 1 і хіт 1 і передана мова
+    public function get_hits($lang, $limit): array
+    {
+        return R::getAll("SELECT p.* , pd.* FROM product p JOIN product_description pd on p.id = pd.product_id 
+                  WHERE p.status = 1 AND p.hit = 1 AND pd.language_id = ? LIMIT $limit", [$lang]);
+    }
 
 }
