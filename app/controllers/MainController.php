54978aaadff82efc2fda66280b52ff4ec6070b8f
Rostyslav Datsko <datskorostislav@gmail.com>
2022-08-07 13:40:46 +0300

    Create wfm/Cache

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use app\models\Main;
 use RedBeanPHP\R;
 use wfm\App;
+use wfm\Cache;
 
 
 /** @property Main $model */
@@ -12,6 +13,12 @@ class MainController extends AppController
 {
     public function indexAction()
     {
+//        $test = 'hi';
+//        $cache = Cache::getInstance();
+//        $cache->set('test', $test, 30);
+//        var_dump($cache->get('test'));
+//        var_dump($test);
+
         $lang = App::$app->getProperty('language');
         $slides = R::findAll('slider');
 
