f0882810278f13418b6a066d035fe8b6a90204a2
Rostyslav Datsko <datskorostislav@gmail.com>
2022-08-09 20:16:24 +0300

    Update product paje (controller; model; language)

---
+++ app/languages/eng/Product/view.php
@@ -0,0 +1,8 @@
+<?php
+
+return[
+    'product_view-buy' => 'Buy',
+    'tpl_is_available' => 'Іs available',
+    'tpl_isn`t_available' => 'Іsn`t available',
+
+];
---
+++ app/languages/ukr/Product/view.php
@@ -0,0 +1,7 @@
+<?php
+
+return[
+    'product_view-buy' => 'Купити',
+    'tpl_is_available' => 'Доступний для покупки',
+    'tpl_isn`t_available' => 'Недоступний для покупки',
+];
