7f2f4d8016bb781096b71534060a1d5e50dd7f57
Rostyslav Datsko <datskorostislav@gmail.com>
2022-09-01 00:00:08 +0300

    Add login

---
+++ app/languages/uk/User/login.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'user_login_login_btn' => 'Ввійти',
+    'user_login_success_login' => 'Ви успішно ввійшли',
+    'user_login_error_login' => 'Логін або пароль введено неправильно',
+];
